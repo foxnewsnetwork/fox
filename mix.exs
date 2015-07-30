ab42c40ff868cf39871a8da9e7feefc8f6a46be6
Thomas Chen <foxnewsnetwork@gmail.com>
2015-07-29 17:19:38 -0700

    bumped version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Fox.Mixfile do
 
   def project do
     [app: :fox,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.0",
      description: "Collection of support utility functions and extensions for day-to-day web development with Elixir",
      name: "fox",
